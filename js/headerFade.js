8ff2eeb7b30ba7a43e9060d2cdb75a3a7fc9e242
Melissa Mattwig <60607975+melissamattwig@users.noreply.github.com>
2023-04-03 21:19:54 -0400

    Update headerFade.js
    change direction of nav bar fade

--- js/headerFade.js
+++ js/headerFade.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {
 
-    if ($(this).scrollTop() < 450) {
+    if ($(this).scrollTop() > 450) {
        $('nav').stop().fadeIn(800); 
    } else {$('nav').stop().fadeOut(800);
     };
